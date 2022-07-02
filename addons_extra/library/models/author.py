b56af69ee603f2d1cbc7a7587f1bbab8550133e3
salehpourts <m.salehpour.takrosystem@gmail.com>
2022-07-02 17:00:33 +0430

    1401-04-11 add

--- addons_extra/library/models/author.py
+++ addons_extra/library/models/author.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from tracemalloc import DomainFilter
 from odoo import fields, models
 
 
@@ -10,3 +9,8 @@ class Author(models.Model):
     
     author_book_ids = fields.One2many('library.author.book', 'author_id', string='Author Books')
     
+    # author_book_ids = fields.One2many('library.author.book', 'author_id', string='Author Books', compute="compute_author_books")
+    
+    # def compute_author_books(self):
+    #     for rec in self:
+    #         rec.author_book_ids = len(self.env['library.book'].search([('author_ids', '=', rec.id)]).mapped("author_id"))
